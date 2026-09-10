22ef18a93c36b9b47ba05af10ea9c63b4317b36d
Sunil Sharma <glsunil@amazon.com>
2026-09-10 09:22:37 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <Image
-      src="/DTIG-prefab2x2-owlDanielElania-v1.4.png"
+      src="/20mb.jpg"
       width={1024}
       height={768}
       quality={75}
