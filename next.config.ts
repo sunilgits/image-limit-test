d54253a4d5bb9d7f927ac3fb5d0ed4352eae2883
Sunil Sharma <glsunil@amazon.com>
2026-09-10 08:25:56 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'your-image-host.example.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
